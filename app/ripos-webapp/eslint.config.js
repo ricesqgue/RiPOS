e337463b1d7871b9b38d23d53c885dd285c48708
Ricardo Esqueda <ricardoeg66@gmail.com>
2025-04-02 10:54:18 -0600

    react app created

---
+++ app/ripos-webapp/eslint.config.js
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import prettierConfig from 'eslint-config-prettier/flat';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+  {
+    settings: {
+      files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+      react: {
+        version: 'detect',
+      },
+      ...pluginReact.configs.flat.recommended, // This is not a plugin object, but a shareable config object
+    },
+  },
+  pluginReact.configs.flat['jsx-runtime'],
+  pluginReactRefresh.configs.recommended,
+  pluginReactHooks.configs['recommended-latest'],
+  pluginPrettierRecommended,
+  {
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error',
+    },
+  },
+  prettierConfig
+);
